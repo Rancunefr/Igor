33ef39327724ddf5fc4b8c4f9fe5fdc770d8dac4
Rancune <rancune@rancune.org>
2022-11-04 01:17:50 +0100

    premiere version du parsing

--- parse_msg.c
+++ parse_msg.c
@@ -1,30 +1,43 @@
+#include <string.h>
+#include "config.h"
 #include "irc.h"
 #include "parse_msg.h"
 
 void parse_msg( connexion_t* server, 
 		char* prefix, char* channel, char* params ) {
 
+
+printf("DEBUG: <%s> \n", params ) ;
+
 	if ( strlen(params) == 0 )
 		return ;
 
 	// La chaine commence par !  => C'est une commande
-	if ( params[0]='!' ) {
+	
+	if ( params[0] == '!' ) {
 		char *p ;
 		char *keyword ;
 		keyword = params+1 ;
 		p = keyword ;
-		while (*p && *p != ' ' )
+		while (*p && *p !=' '&& *p!='\n' && *p!= '\r' )
 			p++ ;
 		*p ='\0' ;
 		
 		if (strcmp(keyword, "help")==0) {
 			irc_say( server, channel, "Bonjour !" ) ;
 		}	
+	
+		// TODO Parsing de fichier ini avec les commandes et leur mot clé
 
-
+		return ;
 	}
 
-	// La chaine contient le mot Igor ( attention CaseSen. )
+	// La chaine transmise contient le nom du bot
+	
+	if ( strstr( params, NICKNAME ) != 0 ) {
+			irc_say( server, channel, "Oui maître, bien maître ..." ) ;
+			return ;
+	}
 	
 
 }
