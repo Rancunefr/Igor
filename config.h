baf150bd4666e1e5f11c5bfd213af3a58f4d8f9e
Rancune <rancune@rancune.org>
2024-05-02 00:37:33 +0200

    oups

--- config.h
+++
@@ -1,6 +0,0 @@
-#define NICKNAME	"igor_le_servile"
-#define ID_TOKEN	"oauth:o2elvsyma0y2r0qcqhkgzqzq5fifrg"
-#define SERVER		"irc.chat.twitch.tv"
-#define PORT		6697
-#define CHANNEL		"#rancune_"
-#define OWNER		"rancune_"
